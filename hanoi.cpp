3f40ad786140222dcb40efa90dde2954db6f5631
Ana Alice <anaalicefm18@gmail.com>
2019-08-24 00:09:26 -0300

    correçao de erros e iniciando aplicaçao

--- hanoi.cpp
+++ hanoi.cpp
@@ -1,40 +1,12 @@
 #include <iostream>
+#include <string> 
 
 using namespace std;
 
-// void renderiza(string[] aneis);
-#defina LINHA = 5;
-#defina COLUNA = 11;
+#define LINHA 5
+#define COLUNA 11
 
-string aneis[linha][coluna];
-
-int main() 
-{    
-  string anelGraph;
-  int posicao1, posicao2, anelInt;
-  bool venceu = false;
-
-  preencheMapa(aneis);
-  renderiza(aneis);
-
-  while(!venceu){
-
-    cout << "Peças: O, o, * " << endl;
-    cout << "Digite a linha, coluna, e peça que deseja mover: " << endl;
-    cin >> posicao1;
-    cin >> posicao2;
-    cin >> anelGraph;
-    anelInt = anelGraphToAnelInt(anelGraph);
-
-    aneis = efetuaJogada(posicao1, posicao2, anelInt);
-    renderiza();
-
-    if(verificaVitoria()){
-      cout << "PARABÉNS!! VOCÊ VENCEU O JOGO" << endl;
-      venceu = true;
-    }
-  }
-}
+string aneis[LINHA][COLUNA];
 
 void renderiza()
 {
@@ -57,37 +29,41 @@ void preencheMapa()
       aneis[i][j] = "-";
     }
   }
-
-  return aneis; 
 }
 
-string efetuaJogada(int posicao1, int posicao2, string aneis[LINHA][COLUNA], int anel)
+bool jogadaValidate(int posicao1, int posicao2, int anel)
 {
-  bool validate = jogadaValidate(posicao, aneis);
-  
-  if(validate) {
-    aneis[posicao1-1][posicao2-1] = anel;
-    return aneis;
-  } else {
-    cout << "JOGADA INVÁLIDA, JOGUE NOVAMENTE" << endl;
-    return aneis;
-  }
-}
+  // stringstream posicaoConv(aneis[posicao1][posicao2-1])
 
-void jogadaValidate(int posicao1, int posicao2, , int anel)
-{
+  int posicao = stoi(aneis[posicao1][posicao2-1]);
+  // posicaoConv >> posicao;
+
   if((posicao1 > 5 || posicao1 < 0) || (posicao2 > 11 || posicao2 < 0))
     return false;
-  else if(aneis[posicao1][posicao2-1] < anel)
+  else if( posicao < anel)
     return false;
     
   for(int i = 0; i < 5; i++){
     for(int j = 0; j < 11; j++){    
       if((j != 2 || j != 5 ||j != 8) && (i == 0 && i == 4))
-      return false;
+        return false;
     }
   }
+
   return true;
+}
+
+bool efetuaJogada(int posicao1, int posicao2, int anelInt)
+{
+  bool validate = jogadaValidate(posicao1, posicao2, anelInt);
+  
+  if(validate) {
+    aneis[posicao1-1][posicao2-1] = anelInt;
+    return true;
+  } else {
+    cout << "JOGADA INVÁLIDA, JOGUE NOVAMENTE" << endl;
+    return false;
+  }
 }
 
 int anelGraphToAnelInt(string anelGraph)
@@ -106,4 +82,33 @@ bool verificaVitoria()
     return true;
 
   return false;
+}
+
+int main() 
+{    
+  string anelGraph;
+  int posicao1, posicao2, anelInt=0;
+  bool venceu = false;
+
+  preencheMapa();
+  renderiza();
+
+  while(!venceu){
+
+    cout << "Peças: O, o, * " << endl;
+    cout << "Digite a linha, coluna, e peça que deseja mover: " << endl;
+    cin >> posicao1;
+    cin >> posicao2;
+    cin >> anelGraph;
+
+    anelInt = anelGraphToAnelInt(anelGraph);
+    
+    efetuaJogada(posicao1, posicao2, anelInt);
+    renderiza();
+
+    if(verificaVitoria()){
+      cout << "PARABÉNS!! VOCÊ VENCEU O JOGO" << endl;
+      venceu = true;
+    }
+  }
 }
